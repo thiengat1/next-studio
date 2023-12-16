23bb96448d3937aea234f9a04290ab0dc28a00db
lewis <thiengat1@gmail.com>
2023-12-16 16:46:42 +0700

    fix for build

--- app/components/Animation/page.tsx
+++ app/components/Animation/page.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-export interface IAnimationProps {
-  children: any;
-}
-
-function Animation({ children }: IAnimationProps) {
+function Animation({ children }) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 200 }}
